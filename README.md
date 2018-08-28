022b8ca26b277bc439c3e6762873ded1bbfa2364
Pradeep Kumar Tippa <pktippa@gmail.com>
2018-08-28 14:52:56 +0530

    Updated the code with latest express and body parser

--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 Express Test run - Get Response
 
 node server.js
+
+## Getting started
+
+1. Clone repo
+2. Install dependencies
+
+```sh
+$ npm install
+```
+
+3. Start Server
+
+```sh
+$ npm start
+```
+
+4. Exposed APIs are
+
+```
+GET
+http://localhost:8080/getResponse
+```
+
+```sh
+POST
+body:
+{
+  id: 1,
+  value: 'post'
+}
+
+http://localhost:8080/postResponse
+```
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 /**
  * New node file
  */
-var express = require('express');
-var parser = require('body-parser');
+const express = require('express');
+var bodyParser = require('body-parser');
 var response = require('./response');
 //console.log("response " + response.response);
-var app = express();
-app.use(parser.json());
-app.use(parser.urlencoded());
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 //curl -v X GET "http://localhost:8080/getResponse"
 app.get("/getResponse",function(req, res) {
@@ -19,4 +22,4 @@ app.post("/postResponse",function(req, res) {
 	console.log("post body : "+JSON.stringify(req.body));  
 	res.jsonp( response.postResponse);
 });
-app.listen(8080);
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
